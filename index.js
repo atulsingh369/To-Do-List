495373bfafab90381d231c0085bd75a11707b381
atulsingh369 <businessatul7684@gmail.com>
2023-02-24 03:59:26 +0530

    new

--- index.js
+++ index.js
@@ -1,23 +1,15 @@
 const img = document.getElementById("img");
 const disp = document.getElementById("taskDisp");
 const input = document.getElementById("taskInput");
-let delImg = document.createElement("img");
-delImg.src = "delete.png";
-
-delImg.addEventListener('click', (e) => {
-	let child = disp.lastElementChild;
-	disp.removeChild(child);
-	if (disp.lastElementChild === img)
-		img.style.display = "flex";
-});
 
 const del = () => {
 	let child = disp.lastElementChild;
+	console.log(child);
 	disp.removeChild(child);
 	if (disp.lastElementChild === img)
 		img.style.display = "flex";
 }
-	
+
 
 const add = () => {
 	img.style.display = "none";
@@ -29,23 +21,27 @@ const add = () => {
 	let main = document.createElement('div');
 	let head = document.createElement('h3');
 	let body = document.createElement('p');
+	let btn = document.createElement('button');
 
 
-	main.classList.add("main");
+	main.style.padding = "5px";
+	btn.classList.add("btn2");
+	btn.textContent = "-";
 	task.classList.add("task");
 	head.style.marginBottom = "10px";
 
 
 	task.appendChild(main);
-	task.appendChild(delImg);
+	task.appendChild(btn);
 	main.appendChild(head);
 	main.appendChild(body);
 
-	console.log(main);
 
 	main.children[0].textContent = input1;
 	main.children[1].textContent = input2;
 	disp.appendChild(task);
-	input1 = "";
-	input2 = "";
+	input.children[0].value = "";
+	input.children[1].value = "";
+	let child = disp.lastElementChild;
+	console.log(child);
 } 
